b706f9005244eed38090875aa1dab7a2301dd9b1
Oksana Chorna <ksuchorna@gmail.com>
2020-11-30 12:53:01 +0200

    Дороблено дз 1

--- js/task-1.js
+++ js/task-1.js
@@ -1,5 +1,10 @@
 const name = "Генератор защитного поля";
 let price = 1000;
-// price = 2000;
-const message = `Выбран "${name}", цена за штуку ${price} кредитов`;
+
+let message = `Выбран "${name}", цена за штуку ${price} кредитов`;
+console.log(message);
+
+price = 2000;
+
+message = `Выбран "${name}", цена за штуку ${price} кредитов`;
 console.log(message);
