f922917bcefbc1d8a87b5eedb4e6560969166377
Oksana Chorna <ksuchorna@gmail.com>
2020-12-01 22:44:05 +0200

    Перероблене дз 1_4

--- js/task-4.js
+++ js/task-4.js
@@ -2,14 +2,22 @@ const credits = 23580;
 const pricePerDroid = 3000;
 let totalPrice;
 
-const answer = prompt("Какое количество дроидов Вы хотите купить?");
+let answer;
+do {
+  answer = prompt('Какое количество дроидов Вы хотите купить?');
 
-if (answer === null) {
+  if (answer === null) {
     console.log('Отменено пользователем!');
-} else {
+  } else if (Number(answer) > 0) {
     totalPrice = pricePerDroid * Number(answer);
-   let message = totalPrice > credits
+    let message =
+      totalPrice > credits
         ? 'Недостаточно средств на счету!'
-        : `Вы купили ${answer} дроидов, на счету осталось ${credits - totalPrice} кредитов.`;
+        : `Вы купили ${answer} дроидов, на счету осталось ${
+            credits - totalPrice
+          } кредитов.`;
     alert(message);
-}
+  } else {
+    answer = '';
+  }
+} while (answer === '');
