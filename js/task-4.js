6927eae2eb421c90f16002c5e95015e303093f62
Oksana Chorna <ksuchorna@gmail.com>
2020-12-02 23:35:03 +0200

    Дз 1_4 виправлено згідно зауважень

--- js/task-4.js
+++ js/task-4.js
@@ -2,22 +2,16 @@ const credits = 23580;
 const pricePerDroid = 3000;
 let totalPrice;
 
-let answer;
-do {
-  answer = prompt('Какое количество дроидов Вы хотите купить?');
-
-  if (answer === null) {
-    console.log('Отменено пользователем!');
-  } else if (Number(answer) > 0) {
-    totalPrice = pricePerDroid * Number(answer);
-    let message =
-      totalPrice > credits
-        ? 'Недостаточно средств на счету!'
-        : `Вы купили ${answer} дроидов, на счету осталось ${
-            credits - totalPrice
-          } кредитов.`;
-    alert(message);
-  } else {
-    answer = '';
-  }
-} while (answer === '');
+const answer = prompt('Какое количество дроидов Вы хотите купить?');
+if (answer === null) {
+  console.log('Отменено пользователем!');
+} else {
+  totalPrice = pricePerDroid * Number(answer);
+  let message =
+    totalPrice > credits
+      ? 'Недостаточно средств на счету!'
+      : `Вы купили ${Number(answer)} дроидов, на счету осталось ${
+          credits - totalPrice
+        } кредитов.`;
+  alert(message);
+}
